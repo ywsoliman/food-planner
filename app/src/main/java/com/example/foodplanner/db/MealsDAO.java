afd0cf4aa6dc4b6ee58a4ef03156b528c6e76981
Youssef Waleed <ywsoliman@gmail.com>
2024-02-12 17:19:37 +0200

    Create MealsDAO.java

---
+++ app/src/main/java/com/example/foodplanner/db/MealsDAO.java
@@ -0,0 +1,26 @@
+package com.example.foodplanner.db;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.foodplanner.models.Meal;
+
+import java.util.List;
+
+@Dao
+public interface MealsDAO {
+
+    @Query("SELECT * FROM meals_table")
+    LiveData<List<Meal>> getAllMeals();
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Meal meal);
+
+    @Delete
+    void delete(Meal meal);
+
+}
